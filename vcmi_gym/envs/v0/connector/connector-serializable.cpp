bdb40e26b6f3bcbae78e24f8b98eff749ece68b0
Simeon Manolov <s.manolloff@gmail.com>
2023-10-27 17:30:22 +0300

    Thread / GIL sequence diagram

--- vcmi_gym/envs/v0/connector/connector-serializable.cpp
+++ vcmi_gym/envs/v0/connector/connector-serializable.cpp
@@ -6,146 +6,124 @@
 #include <boost/thread/thread.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/condition_variable.hpp>
+#include <pthread.h>
 
 // #include <Python.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 // #include <boost/asio.hpp>
 
-// WIP version of a connector that modifies a serializable array
-// (one initialized in python via multiprocessing.Array(ctypes.c_int, 5))
+namespace py = pybind11;
 
-void start_vcmi(const std::function<void(int[])> &pycallback, int values[]) {
+struct Action {
+    void setA(const std::string &a_) { a = a_; }
+    void setB(const py::array_t<float> &b_) { b = b_; }
+
+    const std::string &getA() const { return a; }
+    const py::array_t<float> &getB() const { return b; }
+};
+
+struct State {
+    void setA(const std::string &a_) { a = a_; }
+    void setB(const py::array_t<float> &b_) { b = b_; }
+
+    const std::string &getA() const { return a; }
+    const py::array_t<float> &getB() const { return b; }
+};
+
+// this can't be defined here as const, as it will need access to
+// a cond variable which it should modify
+// TODO: what if the cond variable is global?
+// const State act(Action a) {
+//     printf("[CPP] (act) called with action.getA(): %s, action.getB(): ?\n", a.getA());
+// }
+
+void start_vcmi(const std::function<Action(State)> &pycallback) {
     setvbuf(stdout, NULL, _IONBF, 0);
     printf("[CPP] (start_vcmi) called\n");
-    printf("[CPP] (start_vcmi) values[%d, %d, %d, %d, %d]\n", values[0], values[1], values[2], values[3], values[4]);
+    pthread_setname_np("VCMIRoot");
 
-    boost::mutex m;
-    boost::condition_variable cond;
-
-    // Acquire the lock first to prevent it from locking first
-    // (ie. pass an already locked mutex to the lambda)
-    printf("[CPP] (start_vcmi) acquire lock\n");
-    boost::unique_lock<boost::mutex> lock1(m);
-
-
-    // <DEBUG>
-    // this shows the current thread (same as py thread)
-    // already HAS a state, and already HAS a GIL
-    // (requested exactly once, by python itself)
-    /*
-    auto tstate = PyGILState_GetThisThreadState();
-    tstate
-        ? printf("[CPP] (start_vcmi) tstate->gilstate_counter: %d\n", tstate->gilstate_counter)
-        : printf("[CPP] (start_vcmi) tstate NULL\n");
-    */
-    // </DEBUG>
-
-    // <DEBUG>
-    // this shows how acquiring the GIL simply increments
-    // the GIL counter, without doing anything else
-    // See https://github.com/python/cpython/blob/v3.10.13/Python/pystate.c#L1535
-    /*
-    printf("[CPP] (start_vcmi) acquire Python GIL\n");
-    auto gstate = PyGILState_Ensure();
-    tstate = PyGILState_GetThisThreadState();
-    tstate
-        ? printf("[CPP] (start_vcmi) tstate->gilstate_counter: %d\n", tstate->gilstate_counter)
-        : printf("[CPP] (start_vcmi) tstate NULL\n");
-    */
-    // </DEBUG>
-
-    // GIL management: attempt #1: use core CPython functions
-    // https://docs.python.org/3/c-api/init.html#releasing-the-gil-from-extension-code
-    // Save the thread state in a local variable.
-    // Release the global interpreter lock.
-    //
-    // PROBLEM: cpp compiler error for 'static' storage class
-    /*
-    printf("[CPP] (start_vcmi) release Python GIL\n");
-    Py_BEGIN_ALLOW_THREADS
-    */
-
-
-    // GIL management: attempt #2: use pybind11 functions
     // https://pybind11.readthedocs.io/en/stable/advanced/misc.html#global-interpreter-lock-gil
     // GIL is held when called from Python code. Release GIL before
     // calling into (potentially long-running) C++ code
     printf("[CPP] (start_vcmi) release Python GIL\n");
-    pybind11::gil_scoped_release release;
+    py::gil_scoped_release release;
 
-    printf("[CPP] (start_vcmi) boost::thread(lambda)\n");
-    boost::thread([&m, &cond, &pycallback, &values](){
-        printf("[CPP] (lambda) called\n");
-        printf("[CPP] (lambda) values[%d, %d, %d, %d, %d]\n", values[0], values[1], values[2], values[3], values[4]);
+    printf("[CPP] (start_vcmi) boost::thread(lambda_yourTurn)\n");
+    boost::thread([&pycallback]() -> const std::function<State(Action)> {
+        printf("[CPP] (lambda_yourTurn) called\n");
+        printf("[CPP] (lambda_yourTurn) values[%d, %d, %d, %d, %d]\n", values[0], values[1], values[2], values[3], values[4]);
 
-        printf("[CPP] (lambda) acquire lock\n");
+        pthread_setname_np("VCMIClient");
+
+        printf("[CPP] (lambda_yourTurn) acquire lock\n");
         boost::lock_guard<boost::mutex> lock2(m);
 
-        // printf("[CPP] (lambda) sleep 1s\n");
+        // printf("[CPP] (lambda_yourTurn) sleep 1s\n");
         // boost::this_thread::sleep_for(boost::chrono::seconds(1));
 
-        // GIL management: attempt #1 (cont)
-        // https://docs.python.org/3/c-api/init.html#non-python-created-threads
-        /*
-        printf("[CPP] (lambda) acquire Python GIL\n");
-        PyGILState_STATE gstate;
-        gstate = PyGILState_Ensure();
-        */
+        boost::mutex m;
+        boost::condition_variable cond;
 
-        // GIL management: attempt #2 (cont)
+        // Define a callback to be called after in env.step()
+        // TODO: lambda_act
+        auto [&m, &cond]
+
+        // Acquire the lock now to prevent the lambda_act from locking first
+        // (ie. pass an already locked mutex to the lambda_act)
+        printf("[CPP] (lambda_yourTurn) acquire lock\n");
+        boost::unique_lock<boost::mutex> lock(m);
+
         // Acquire the GIL
-        printf("[CPP] (lambda) acquire Python GIL\n");
-        pybind11::gil_scoped_acquire acquire;
+        printf("[CPP] (lambda_yourTurn) acquire Python GIL\n");
+        py::gil_scoped_acquire acquire;
 
         // THIS segfaults!
-        // printf("[CPP] (lambda) set values[3]=69\n");
+        // printf("[CPP] (lambda_yourTurn) set values[3]=69\n");
         // values[4] = 69;
 
-        printf("[CPP] (lambda) call pycallback(values)\n");
+        printf("[CPP] (lambda_yourTurn) call pycallback(values)\n");
         pycallback(values);
 
-        // GIL management: attempt #2 (cont)
-        printf("[CPP] (lambda) release Python GIL\n");
-        pybind11::gil_scoped_release release;
+        printf("[CPP] (lambda_yourTurn) release Python GIL\n");
+        py::gil_scoped_release release;
 
-        // GIL management: attempt #1 (cont)
-        // https://docs.python.org/3/c-api/init.html#non-python-created-threads
-        // Release the thread. No Python API allowed beyond this point.
-        /*
-        printf("[CPP] (lambda) release Python GIL\n");
-        PyGILState_Release(gstate);
-        */
+        // We've set some events in motion:
+        //  - in python, env = Env() has now returned, storing our lambda_act
+        //  - in python, env.step(action) will be called, which will call our lambda_act
+        // our lambda_act will then call AI->cb->makeAction()
+        // ...we wait until that happens, and FINALLY we can return from yourTurn
+        printf("[CPP] (lambda_yourTurn) cond.wait()\n");
+        cond.wait(lock)
 
-        printf("[CPP] (lambda) cond.notify_one()\n");
-        cond.notify_one();
-
-        printf("[CPP] (lambda) return\n");
+        printf("[CPP] (lambda_yourTurn) return\n");
     });
 
-    printf("[CPP] (start_vcmi) sleep 1s\n");
-    boost::this_thread::sleep_for(boost::chrono::seconds(1));
+    // printf("[CPP] (start_vcmi) Reacquire Python GIL\n");
+    // py::gil_scoped_acquire acquire;
 
-    printf("[CPP] (start_vcmi) cond.wait()\n");
-    cond.wait(lock1);
+    printf("[CPP] (start_vcmi) Entering sleep loop...\n");
+    while(true)
+        boost::this_thread::sleep(boost::chrono::seconds(1));
 
-    // GIL management: attempt #1 (cont)
-    // https://docs.python.org/3/c-api/init.html#releasing-the-gil-from-extension-code
-    // Reacquire the global interpreter lock.
-    // Restore the thread state from the local variable.
-    /*
-    printf("[CPP] (start_vcmi) Reacquire Python GIL\n");
-    Py_BEGIN_ALLOW_THREADS
-    */
-
-    // GIL management: attempt #2 (cont)
-    // XXX: the pybind11 examples do not re-acquire the GIL at all?!?
-    printf("[CPP] (start_vcmi) Reacquire Python GIL\n");
-    pybind11::gil_scoped_acquire acquire;
-
-    printf("[CPP] (start_vcmi) return\n");
+    printf("[CPP] (start_vcmi) return !!!! !SHOULD NEVER HAPPEN\n");
 }
 
 PYBIND11_MODULE(connector, m) {
     m.def("start_vcmi", &start_vcmi, "Start VCMI");
+
+    py::class_<Action>(m, "Action")
+        .def(py::init<>())
+        .def("setA", &Pet::setA)
+        .def("getA", &Pet::getA)
+        .def("setB", &Pet::setB)
+        .def("getB", &Pet::getB);
+
+    py::class_<State>(m, "State")
+        .def(py::init<>())
+        .def("setA", &Pet::setA)
+        .def("getA", &Pet::getA)
+        .def("setB", &Pet::setB)
+        .def("getB", &Pet::getB);
 }
--- vcmi_gym/envs/v0/connector/connector-threading.py
+++ vcmi_gym/envs/v0/connector/connector-threading.py
@@ -2,69 +2,113 @@ from build import connector
 import time
 import asyncio
 import threading
+import numpy as np
 
 
-# uses threading, does not work:
+async def act_vcmi(cppcb, action):
+    print("[PY] (act_vcmi) called")
+    event = asyncio.Event()
+    state_wrapper = {"state": "TODO_1", "cppcb": "TODO_1"}
 
-# $ python connector.py
-# [PY] (main) asyncio start
-# [PY] (start_vcmi) called with: event=<asyncio.locks.Event object at 0x105511390 [unset]> | shared_obj=['main']
-# [PY] (start_vcmi) threading.Thread(target=connector.start_vcmi, args=...)
-# ^^^^ thread blocks here (CPP is actually blocking)
-# [CPP] (start_vcmi) called with pycallback=?, value=1
-# [CPP] (start_vcmi) acquire lock
-# [CPP] (start_vcmi) boost::thread(lambda)
-# [CPP] (start_vcmi) sleep 1s
-# [CPP] (lambda) called with value=1
-# [CPP] (lambda) acquire lock
-# [CPP] (start_vcmi) cond.wait()
-# [CPP] (lambda) sleep 1s
-# [CPP] (lambda) call pycallback(value+1)
-# ^^^^ deadlock: CPP waits py and py waits CPP
+    # cb to be called from VCMI client thread
+    def pycallback_1(state, cppcb):
+        print("[PY] (pycallback_1) called with: state=%d, cppcb=%s" % (state, cppcb))
+        print("[PY] (pycallback_1) state.getA(): %s, state.getB(): %s" % state.getA(), state.getB())
 
-async def start_vcmi(event, shared_obj):
-    print("[PY] (start_vcmi) called with: event=%s | shared_obj=%s" % (event, shared_obj))
-    shared_obj[1] = 1
-
-    def pycallback(vcmi_retval):
-        print("[PY] (pycallback) called with: vcmi_retval=%d" % vcmi_retval)
-        shared_obj[2] = 2
-
-        print("[PY] (pycallback) sleep(1)")
+        print("[PY] (pycallback_1) sleep(1)")
         time.sleep(1)
 
-        print("[PY] (pycallback) event.set()")
+        # TODO:
+        # must somehow pass this state to the other thread
+        # however, simply assigning it to a shared variable would
+        # cause segfaults? (unconfirmed)
+        # state_wrapper["state"] = state
+        # state_wrapper["cppcb"] = cppcb
+
+        # state and cppcb are now available => unblock act_vcmi
+        print("[PY] (pycallback_1) event.set()")
         event.set()
-        print("[PY] (pycallback) return")
 
-    print("[PY] (start_vcmi) call connector.start_vcmi(...)")
-    connector.start_vcmi(pycallback, 1)
+        print("[PY] (pycallback_1) return")
 
-    # print("[PY] (start_vcmi) threading.Thread(target=connector.start_vcmi, args=...)")
-    # t = threading.Thread(target=connector.start_vcmi, args=(pycallback, 1))
-    # t.run()
+    cppaction = connector.Action()
+    cppaction.setA(str(action))
+    cppaction.setB(action)
 
+    print("[PY] (act_vcmi) call cppcb(%s, %s)" % (pycallback_1, cppaction))
+    cppcb(pycallback_1, cppaction)
+
+    # wait until cppcb calls pycallback_1, setting state and cppcb
+    print("[PY] (act_vcmi) event.wait()")
+    await event.wait()
+
+    print("[PY] (act_vcmi) state: %s" % state)
+
+    return state["state"], state["cppcb"]
+
+
+async def start_vcmi(event):
+    print("[PY] (start_vcmi) called")
+
+    event = asyncio.Event()
+    state_wrapper = {"state": "TODO", "cppcb": "TODO"}
+
+    # cb to be called from VCMI client thread
+    def pycb(state, cppcb):
+        print("[PY] (pycb) called with: state=%d, cppcb=%s" % (state, cppcb))
+        print("[PY] (pycb) state.getA(): %s, state.getB(): %s" % state.getA(), state.getB())
+
+        print("[PY] (pycb) sleep(1)")
+        time.sleep(1)
+
+        # TODO:
+        # must somehow pass this state to the other thread
+        # however, simply assigning it to a shared variable would
+        # cause segfaults? (unconfirmed)
+        # state_wrapper["state"] = state
+        # state_wrapper["cppcb"] = cppcb
+
+        # state and cppcb are now available => unblock start_vcmi
+        print("[PY] (pycb) event.set()")
+        event.set()
+
+        print("[PY] (pycb) return")
+
+    # print("[PY] (start_vcmi) call connector.start_vcmi(...)")
+    # connector.start_vcmi(pycb)
+
+    print("[PY] (start_vcmi) threading.Thread(target=connector.start_vcmi, args=(pycb))")
+    t = threading.Thread(target=connector.start_vcmi, args=(pycb))
+    t.run()
+
+    # wait until connector.start_vcmi calls pycb, setting state and cppcb
     print("[PY] (start_vcmi) event.wait()")
     await event.wait()
 
-    print("[PY] (start_vcmi) sleep(1)")
-    time.sleep(1)
+    print("[PY] (start_vcmi) state: %s" % state)
 
-    print("[PY] (start_vcmi) return shared_obj")
-    return shared_obj
+    return state["state"], state["cppcb"]
 
 
 def main():
     shared_obj = [0, 0, 0, 0, 0];
-    event = asyncio.Event()
 
+    #
+    # Simulates env = VcmiEnv()
+    #
     print("[PY] (main) asyncio start")
-    retval = asyncio.run(start_vcmi(event, shared_obj))
-
+    state, cppcb = asyncio.run(start_vcmi())
     print("[PY] (main) asyncio done")
-    print("[PY] (main) retval=%s" % retval)
-    print("[PY] (main) shared_obj=%s" % shared_obj)
-    print("[PY] (main) retval is shared_obj: %s" % (retval is shared_obj))
+    print("[PY] (main) state=%s, cppcb=%s" % (state, cppcb))
+
+    #
+    # Simulates 3 steps
+    #
+    for i in range(3):
+        action = np.array([i, i+1, i+2], dtype=np.float32)
+        state, cppcb = asyncio.run(act_vcmi(cppcb, action))
+
+    # return from .step()
 
 
 if __name__ == '__main__':
