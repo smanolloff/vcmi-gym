94588e0129a66207e9202acbc6533fdb6311d806
Simeon Manolov <s.manolloff@gmail.com>
2024-12-10 09:55:24 +0200

    .

--- rl/encoder/autoencoder.py
+++ rl/encoder/autoencoder.py
@@ -65,14 +65,18 @@ class Autoencoder(nn.Module):
         return reconstructed
 
 
-class Logger:
+class StructuredLogger:
     def __init__(self, filename):
         self.filename = filename
         self.log(dict(filename=filename))
 
     def log(self, obj):
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%.6S")
-        log_obj = dict(timestamp=timestamp, message=obj)
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%.3s")
+        if isinstance(obj, dict):
+            log_obj = dict(timestamp=timestamp, message=obj)
+        else:
+            log_obj = dict(timestamp=timestamp, message=dict(string=obj))
+
         print(yaml.dump(log_obj, sort_keys=False))
         with open(self.filename, "a+") as f:
             f.write(json.dumps(log_obj) + "\n")
@@ -113,10 +117,10 @@ def collect_observations(logger, env, buffer, n, progress_report_steps=0):
         buffer.add(obs)
 
         # Ensure logging on final obs
-        progress = 2 if i == n else i / n
+        progress = round((i+1) / n, 3)
         if progress >= next_progress_report_at:
             next_progress_report_at += progress_report_step
-            logger.log(dict(observations_collected=i/n, progress=round(progress*100), terms=terms, truncs=truncs))
+            logger.log(dict(observations_collected=i/n, progress=progress*100, terms=terms, truncs=truncs))
 
 
 def train_autoencoder(
@@ -124,15 +128,16 @@ def train_autoencoder(
     autoencoder,
     optimizer,
     buffer,
-    batch_size,
     train_epochs,
-    train_steps
+    train_batch_size
 ):
     autoencoder.train()
+    train_steps = buffer.capacity // train_batch_size
+
     for epoch in range(train_epochs):
         loss_sum = 0
         for _ in range(train_steps):
-            batch = buffer.sample(batch_size)
+            batch = buffer.sample(train_batch_size)
             reconstructed = autoencoder(batch)
             loss = mse_loss(reconstructed, batch)
             loss_sum += loss.item()
@@ -141,23 +146,23 @@ def train_autoencoder(
             optimizer.step()
 
         loss = loss_sum / train_steps
-        logger.log(dict(train_epoch=epoch+1, epochs_total=train_epochs, loss=round(loss, 6)))
+        logger.log(dict(train_epoch=epoch, loss=round(loss, 6)))
 
 
-def eval_autoencoder(logger, autoencoder, buffer, eval_steps, batch_size):
+def eval_autoencoder(logger, autoencoder, buffer, eval_env_steps):
     autoencoder.eval()
     loss_sum = 0
-    for _ in range(eval_steps):
+    batch_size = eval_env_steps // 10
+    for _ in range(10):
         batch = buffer.sample(batch_size)
         reconstructed = autoencoder(batch)
         loss_sum += mse_loss(reconstructed, batch).item()
 
-    return loss_sum / eval_steps
+    return loss_sum / eval_env_steps
 
 
 def main():
-    chars = string.ascii_letters + string.digits
-    run_id = ''.join(random.choice(chars) for _ in range(8))
+    run_id = ''.join(random.choices(string.ascii_lowercase, k=8))
 
     config = dict(
         run=dict(
@@ -185,29 +190,30 @@ def main():
             learning_rate=1e-4,
             buffer_capacity=100_000,
             train_epochs=10,
-            train_steps=100,
-            eval_steps=10_000,
+            train_batch_size=100,
+            eval_env_steps=10_000,
+            # buffer_capacity=100,
+            # train_epochs=2,
+            # train_batch_size=10,
+            # eval_env_steps=100,
         )
     )
 
     os.makedirs(config["run"]["out_dir"], exist_ok=True)
-    logger = Logger(filename=os.path.join(config["run"]["out_dir"], f"{run_id}.log"))
+    logger = StructuredLogger(filename=os.path.join(config["run"]["out_dir"], f"{run_id}.log"))
     logger.log(dict(config=config))
-
-    # Initialize environment, buffer, and model
-    env = VcmiEnv_v5(**config["env"])
 
     learning_rate = config["train"]["learning_rate"]
     buffer_capacity = config["train"]["buffer_capacity"]
     train_epochs = config["train"]["train_epochs"]
-    train_steps = config["train"]["train_steps"]
-    eval_steps = config["train"]["eval_steps"]
+    train_batch_size = config["train"]["train_batch_size"]
+    eval_env_steps = config["train"]["eval_env_steps"]
 
-    steps_per_epoch = train_epochs * train_steps
-    assert buffer_capacity % steps_per_epoch == 0
-    assert buffer_capacity % eval_steps == 0
-    batch_size = buffer_capacity // steps_per_epoch
-    eval_batch_size = buffer_capacity // eval_steps
+    assert buffer_capacity % train_batch_size == 0  # needed for train_steps
+    assert eval_env_steps % 10 == 0  # needed for eval batch_size
+
+    # Initialize environment, buffer, and model
+    env = VcmiEnv_v5(**config["env"])
 
     dict_obs, _ = env.reset()
     obs = to_tensor(dict_obs)
@@ -215,7 +221,7 @@ def main():
     buffer = Buffer(capacity=buffer_capacity, obs_shape=obs.shape, device="cpu")
     optimizer = torch.optim.Adam(ae.parameters(), lr=learning_rate)
 
-    iteration = 1
+    iteration = 0
     while True:
         collect_observations(
             logger=logger,
@@ -230,8 +236,7 @@ def main():
             logger=logger,
             autoencoder=ae,
             buffer=buffer,
-            eval_steps=eval_steps,
-            batch_size=eval_batch_size
+            eval_env_steps=eval_env_steps,
         )
 
         logger.log(dict(iteration=iteration, eval_loss=round(loss, 6)))
@@ -241,11 +246,13 @@ def main():
             autoencoder=ae,
             optimizer=optimizer,
             buffer=buffer,
-            batch_size=batch_size,
             train_epochs=train_epochs,
-            train_steps=train_steps
+            train_batch_size=train_batch_size
         )
 
+        filename = os.path.join(config["run"]["out_dir"], f"{run_id}.pt")
+        logger.log(f"Saving model to {filename}")
+        torch.save(ae, filename)
 
 
 if __name__ == "__main__":
