9dfd50fd7afc94c322df400cf982470274f1fc88
Simeon Manolov <simeon.manolov@gate-ai.eu>
2025-04-17 14:32:59 +0300

    fix p10n loss calc

--- rl/p10n/action/classic_int/main.py
+++ rl/p10n/action/classic_int/main.py
@@ -43,11 +43,9 @@ DIM_OBS = DIM_OTHER + DIM_HEXES
 
 
 class MainAction(enum.IntEnum):
-    WAIT = 0
+    RESET = 0
+    WAIT = enum.auto()
     HEX = enum.auto()
-    DONE = enum.auto()
-
-    _count = enum.auto()
 
 
 def wandb_log(*args, **kwargs):
@@ -342,7 +340,7 @@ class ActionPredictionModel(nn.Module):
         #
 
         # => (B, Z_AGG)
-        self.head_main = nn.LazyLinear(MainAction._count)
+        self.head_main = nn.LazyLinear(len(MainAction))
         # => (B, MainAction._count)
 
         # => (B, 165, Z_AGG + Z_HEX)
@@ -413,21 +411,20 @@ class ActionPredictionModel(nn.Module):
         return main_out, hex_out
 
     # Predict next obs
-    def predict(self, obs, action):
+    def predict(self, obs):
         with torch.no_grad():
             obs = torch.tensor(obs, dtype=torch.float32, device=self.device).unsqueeze(0)
-            action = torch.tensor(action, dtype=torch.int64, device=self.device).unsqueeze(0)
 
             was_training = self.training
             self.eval()
             try:
-                main_logits, hex_logits = self.forward(obs, action)
+                main_logits, hex_logits = self.forward(obs)
             finally:
                 self.train(was_training)
 
             main_action = main_logits[0].argmax()
 
-            if main_action == MainAction.DONE:
+            if main_action == MainAction.RESET:
                 return 0
             elif main_action == MainAction.WAIT:
                 return 1
@@ -766,7 +763,7 @@ def compute_loss(action, logits_main, logits_hex):
     # self.where(condition, y) is equivalent to torch.where(condition, self, y)
     target_main = (
         action
-        .where(action != -1, MainAction.DONE)   # -1 => DONE
+        .where(action != -1, MainAction.RESET)  # -1 => RESET (e.g. done=True)
         .where(action != 1, MainAction.WAIT)    # 1 => WAIT
         .where(action < 2, MainAction.HEX)      # 2+ => HEX
     )
@@ -781,9 +778,9 @@ def compute_loss(action, logits_main, logits_hex):
     # Loss for HEX actions (if any)
     #
 
-    # Get relevant action indexes
+    # Get relevant indexes of actions what are on a hex
     # (B) of actions
-    hex_action_inds = torch.where(action >= 2)[0]
+    actions_on_hex = torch.where(action >= 2)[0]
     # => (B') of indexes
 
     # logits_hex is (B, 165, 1 + N_HEX_ACTIONS)
@@ -791,18 +788,18 @@ def compute_loss(action, logits_main, logits_hex):
     # The CE target will be the real, numeric hex id
     # i.e. form a single one-hot categorical from the 165 score logits
     #      and compare it against a numeric hex ID
-    logits_hex_score = logits_hex[hex_action_inds, :, 0]
+    logits_hex_score = logits_hex[actions_on_hex, :, 0]
     # => (B', 165) of score logits
-    target_hex_id = (action[hex_action_inds] - 2) // 165
+    target_hex_id = (action[actions_on_hex] - 2) // len(HEX_ACT_MAP)
     # (B') of hex_ids
     loss_hex = cross_entropy(logits_hex_score, target_hex_id)
 
     # logits_hex is (B, 165, 1 + N_HEX_ACTIONS)
     # The CE logits will be the hexaction logits (>1st logit) of the target hex
     # The CE target will be the real, numeric hex action for the target hex
-    logits_hex_action = logits_hex[hex_action_inds, target_hex_id, 1:]
+    logits_hex_action = logits_hex[actions_on_hex, target_hex_id, 1:]
     # => (B', N_HEX_ACTIONS) of hexaction logits for the target hex
-    target_hex_action = (action[hex_action_inds] - 2) % len(HEX_ACT_MAP)
+    target_hex_action = (action[actions_on_hex] - 2) % len(HEX_ACT_MAP)
     # (B') of hex_actions
     loss_hexaction = cross_entropy(logits_hex_action, target_hex_action)
 
@@ -1632,7 +1629,8 @@ def test(cfg_file):
     run_id = os.path.basename(cfg_file).removesuffix("-config.json")
     weights_file = f"data/t10n/{run_id}-model.pt"
     model = load_for_test(weights_file)
-    env = VcmiEnv(mapname="gym/generated/4096/4x1024.vmap", conntype="thread", random_heroes=1, swap_sides=1)
+    # env = VcmiEnv(mapname="gym/generated/4096/4x1024.vmap", conntype="thread", random_heroes=1, swap_sides=1)
+    env = VcmiEnv(mapname="gym/generated/evaluation/8x512.vmap", conntype="thread", random_heroes=1, opponent="BattleAI")
     do_test(model, env)
 
 
@@ -1648,113 +1646,67 @@ def load_for_test(file):
 def do_test(model, env):
     from vcmi_gym.envs.v10.decoder.decoder import Decoder
 
+    t = lambda x: torch.as_tensor(x).unsqueeze(0)
+
     env.reset()
     for _ in range(10):
-        print("=" * 100)
         if env.terminated or env.truncated:
             env.reset()
         act = env.random_action()
-        obs, rew, term, trunc, _info = env.step(act)
+        obs0, rew, term, trunc, _info = env.step(act)
+        done = term or trunc
 
-        # [(obs, act, real_obs), (obs, act, real_obs), ...]
-        dream = [(obs["transitions"]["observations"][0], obs["transitions"]["actions"][0], None)]
+        num_transitions = len(obs0["transitions"]["observations"])
+        for i in range(1, num_transitions):
+            obs = obs0["transitions"]["observations"][i]
+            act = obs0["transitions"]["actions"][i]
 
-        for i in range(1, len(obs["transitions"]["observations"])):
-            obs_prev = obs["transitions"]["observations"][i-1]
-            act_prev = obs["transitions"]["actions"][i-1]
-            obs_next = obs["transitions"]["observations"][i]
-            # mask_next = obs["transitions"]["action_masks"][i]
-            # rew_next = obs["transitions"]["rewards"][i]
-            # done_next = (term or trunc) and i == len(obs["transitions"]["observations"]) - 1
+            if act == -1:
+                # Usually, act=-1 on every *last* transition
+                # We care about it only if it's the last for the entire episode
+                assert i == num_transitions - 1, f"{i} == {num_transitions} - 1"
+                if not done:
+                    continue
 
-            obs_pred_raw = model(torch.as_tensor(obs_prev).unsqueeze(0), torch.as_tensor(act_prev).unsqueeze(0))
-            obs_pred_raw = obs_pred_raw[0]
-            obs_pred = model.predict(obs_prev, act_prev)
-            dream.append((model.predict(*dream[i-1][:2]), obs["transitions"]["actions"][i], obs_next))
+            logits_main, logits_hex = model(t(obs))
+            act_pred = model.predict(obs)
 
-            def prepare(state, action, reward, headline):
-                import re
-                bf = Decoder.decode(state)
-                ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-                rewtxt = "" if reward is None else "Reward: %s" % round(reward, 2)
-                render = {}
-                render["bf_lines"] = bf.render_battlefield()[0][:-1]
-                render["bf_len"] = [len(l) for l in render["bf_lines"]]
-                render["bf_printlen"] = [len(ansi_escape.sub('', l)) for l in render["bf_lines"]]
-                render["bf_maxlen"] = max(render["bf_len"])
-                render["bf_maxprintlen"] = max(render["bf_printlen"])
-                render["bf_lines"].insert(0, rewtxt.ljust(render["bf_maxprintlen"]))
-                render["bf_printlen"].insert(0, len(render["bf_lines"][0]))
-                render["bf_lines"].insert(0, headline)
-                render["bf_printlen"].insert(0, len(render["bf_lines"][0]))
-                render["bf_lines"] = [l + " "*(render["bf_maxprintlen"] - pl) for l, pl in zip(render["bf_lines"], render["bf_printlen"])]
-                render["bf_lines"].append(env.__class__.action_text(action, bf=bf).rjust(render["bf_maxprintlen"]))
-                return render["bf_lines"]
+            bf = Decoder.decode(obs)
+            print(bf.render(0))
 
-            lines_prev = prepare(obs_prev, act_prev, None, "Start:")
-            lines_real = prepare(obs_next, -1, None, "Real:")
-            lines_pred = prepare(obs_pred, -1, None, "Predicted:")
+            probs_main = logits_main[0].softmax(0)
+            probs_hex = logits_hex[0, :, 0].softmax(0)
 
-            # TODO
-            # losses = compute_loss(
-            # )
+            # Probs + indices for top 3 hexes
+            k = 3
+            topk_hexes = probs_hex.topk(k)
+            # => (k,)
 
-            # print("Losses | Obs: binary=%.4f, cont=%.4f, categorical=%.4f" % losses)
+            # Probs + indices for all 14 actions of the top 3 hexes
+            probs_actions_topk_hexes = logits_hex[0, topk_hexes.indices, 1:].softmax(1)
+            # => (k, N_HEX_ACTIONS)
 
-            # print(Decoder.decode(obs_prev).render(0))
-            # for i in range(len(bfields)):
-            print("")
-            print("\n".join([(" ".join(rowlines)) for rowlines in zip(lines_prev, lines_real, lines_pred)]))
-            print("")
+            topk_hexes_topk_actions = probs_actions_topk_hexes.topk(k, dim=1)
+            # => (k, k)  # dim0=hexes, dim1=actions
 
-            # bf_next = Decoder.decode(obs_next)
-            # bf_pred = Decoder.decode(obs_pred)
+            hexactnames = list(HEX_ACT_MAP.keys())
 
-            # print(env.render_transitions())
-            # print("Predicted:")
-            # print(bf_pred.render(0))
-            # print("Real:")
-            # print(bf_next.render(0))
+            print("Real action: %d (%s)" % (act, env.action_text(act, bf=bf)))
+            print("Pred action: %d (%s)" % (act_pred, env.action_text(act_pred, bf=bf)))
+            print("Probs:")
 
-            # hex20_pred.stack.QUEUE.raw
+            assert MainAction.HEX == len(MainAction) - 1
+            for ma in MainAction:
+                print(" * %s: %.2f" % (ma.name, probs_main[ma.value]))
 
-            # def action_str(obs, a):
-            #     if a > 1:
-            #         bf = Decoder.decode(obs)
-            #         hex = bf.get_hex((a - 2) // len(HEX_ACT_MAP))
-            #         act = list(HEX_ACT_MAP)[(a - 2) % len(HEX_ACT_MAP)]
-            #         return "%s (y=%s x=%s)" % (act, hex.Y_COORD.v, hex.X_COORD.v)
-            #     else:
-            #         assert a == 1
-            #         return "Wait"
+            for i in range(k):
+                hex = bf.get_hex(topk_hexes.indices[i].item())
+                hex_desc = "Hex(y=%s x=%s)/%.2f" % (hex.Y_COORD.v, hex.X_COORD.v, topk_hexes.values[i])
+                hex_actions = "".join([f"{hexactnames[ind.item()]}/{prob.item():.2f}".ljust(15) for ind, prob in zip(topk_hexes_topk_actions.indices[i], topk_hexes_topk_actions.values[i])])
+                print("    - k=%d: %-18s => %s" % (i, hex_desc, hex_actions))
 
-        if len(dream) > 2:
-            print(" ******** SEQUENCE: ********** ")
-            print(env.render_transitions())
-            print(" ******** DREAM: ********** ")
-            rcfg = env.reward_cfg._replace(step_fixed=0)
-            for i, (obs, act, obs_real) in enumerate(dream):
-                print("*" * 10)
-                if i == 0:
-                    print("Start:")
-                    print(Decoder.decode(obs).render(act))
-                else:
-                    bf_real = Decoder.decode(obs_real)
-                    bf = Decoder.decode(obs)
-                    print(f"Real step #{i}:")
-                    print(bf_real.render(act))
-                    print("")
-                    print(f"Dream step #{i}:")
-                    print(bf.render(act))
-                    print(f"Real / Dream rewards: {env.calc_reward(0, bf_real, rcfg)} / {env.calc_reward(0, bf, rcfg)}:")
-
-
-    # print(env.render_transitions())
-
-    # print("Pred:")
-    # print(Decoder.decode(obs_pred))
-    # print("Real:")
-    # print(Decoder.decode(obs_real))
+            import ipdb; ipdb.set_trace()  # noqa
+            print("==========================================================")
 
 
 if __name__ == "__main__":
@@ -1765,21 +1717,6 @@ if __name__ == "__main__":
     parser.add_argument("--loglevel", metavar="LOGLEVEL", default="INFO", help="DEBUG | INFO | WARN | ERROR")
     parser.add_argument('action', metavar="ACTION", type=str, help="train | test | sample")
     args = parser.parse_args()
-
-    # # XXXX: TEST
-    # device = torch.device("cpu")
-    # model = TransitionModel(device=device)
-    # next_obs = model.reconstruct(torch.randn([2, DIM_OBS], device=device))
-    # pred_obs = model.forward(next_obs, torch.tensor([1, 1], device=device))
-    # compute_losses(
-    #     None,
-    #     model.obs_index,
-    #     compute_loss_weights(Stats(model, device=device), device=device),
-    #     next_obs,
-    #     pred_obs
-    # )
-    # assert 0
-    # # XXXXX: EOF: TEST
 
     if args.dry_run:
         args.no_wandb = True
