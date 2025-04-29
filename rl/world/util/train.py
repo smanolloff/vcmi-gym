a380318eed399f81fe440df72f846691a34bc3ad
Simeon Manolov <simeon.manolov@gate-ai.eu>
2025-04-29 13:03:19 +0300

    fix pandas error

--- rl/world/util/train.py
+++ rl/world/util/train.py
@@ -257,7 +257,8 @@ def train(
             df[df[TableColumn.STAGE] == stage]
             .groupby([TableColumn.ATTRIBUTE, TableColumn.CONTEXT, TableColumn.DATATYPE])[TableColumn.LOSS]
             .mean()  # mean by attribute
-            .groupby([TableColumn.CONTEXT, TableColumn.DATATYPE])[TableColumn.LOSS]
+            # => dict with keys = tuple(attrname, context, datatype), values = float(loss)
+            .groupby([TableColumn.CONTEXT, TableColumn.DATATYPE])  # NOTE: no column LOSS anymore
             .sum()   # sum by context/datatype
             # => dict with keys = tuple(context, datatype), values = float(loss)
         )
