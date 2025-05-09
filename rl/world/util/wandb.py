868d480681c86ac5d8dd77a353ee4066d4f5f984
Simeon Manolov <simeon.manolov@gate-ai.eu>
2025-05-09 20:25:31 +0300

    add start_infos in wandb config

--- rl/world/util/wandb.py
+++ rl/world/util/wandb.py
@@ -1,10 +1,29 @@
-import pathlib
+import pygit2
+import os
+import tempfile
+from datetime import datetime
 
 
 def setup_wandb(config, model, src_file):
     import wandb
 
     resumed = config["run"]["resumed_config"] is not None
+
+    git = pygit2.Repository(os.path.dirname(__file__))
+    now = datetime.utcnow()
+    patch = git.diff().patch
+
+    start_info = dict(
+        git_head=str(git.head.target),
+        git_status={k: v.name for k, v in git.status().items()},
+        timestamp=now.isoformat(timespec='milliseconds'),
+        vastai_instance_id=os.getenv("VASTAI_INSTANCE_ID"),
+    )
+
+    if patch:
+        start_info.update(git_is_dirty=True, git_diff_artifact="gitdiff-%d" % now.timestamp())
+    else:
+        start_info.update(git_is_dirty=False)
 
     wandb.init(
         project="vcmi-gym",
@@ -17,18 +36,26 @@ def setup_wandb(config, model, src_file):
         sync_tensorboard=False,
         save_code=False,  # code saved manually below
         allow_val_change=resumed,
-        settings=wandb.Settings(_disable_stats=True, _disable_meta=True),  # disable System/ stats
+        # settings=wandb.Settings(_disable_stats=True),  # disable System/ stats
     )
 
-    # https://docs.wandb.ai/ref/python/run#log_code
-    # XXX: "path" is relative to `root`
-    #      but args.cfg_file is relative to vcmi-gym ROOT dir
-    src_file = pathlib.Path(src_file)
+    start_infos = wandb.config.get("_start_infos", [])
+    start_infos.append(start_info)
+    wandb.config.update(dict(_start_infos=start_infos), allow_val_change=True)
 
-    def code_include_fn(path):
-        p = pathlib.Path(path).absolute()
-        return p.samefile(src_file)
+    # Must be after wandb.init
+    if start_info["git_is_dirty"]:
+        art = wandb.Artifact(name=start_info["git_diff_artifact"], type="text")
+        art.description = f"Git diff for HEAD@{start_info['git_head']} from {start_info['timestamp']}"
+        with tempfile.NamedTemporaryFile(mode='w+', delete=True) as temp_file:
+            art.metadata["timestsamp"] = start_info["timestamp"]
+            art.metadata["head"] = start_info["git_head"]
+            temp_file.write(f"# Head: {start_info['git_head']}\n")
+            temp_file.write(f"# Timestamp: {start_info['timestamp']}\n")
+            temp_file.write(patch)
+            temp_file.flush()
+            art.add_file(temp_file.name, name="diff.patch")
+            wandb.run.log_artifact(art)
 
-    wandb.run.log_code(root=src_file.parent, include_fn=code_include_fn)
     wandb.watch(model, log="all", log_graph=True, log_freq=1000)
     return wandb
