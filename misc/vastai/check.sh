4e1d6057d89d03fcfed215ee9e5b25f05325b4db
Simeon Manolov <s.manolloff@gmail.com>
2026-03-18 23:07:52 +0200

    fix check

--- misc/vastai/check.sh
+++ misc/vastai/check.sh
@@ -3,9 +3,12 @@
 set -euxo pipefail
 
 N_ROLLOUTS=5
+INIT_SECONDS=30
 ROLLOUT_SECONDS=27
-SECONDS_MAX=$((N_ROLLOUTS * ROLLOUT_SECONDS))
+SECONDS_OK=$((N_ROLLOUTS * ROLLOUT_SECONDS))
+SECONDS_TIMEOUT=$((INIT_SECONDS + SECONDS_MAX * 2))
 
+. ~/.simorc
 cd /workspace/vcmi-gym
 
 download_checkpoint ytoowqgj-1773645017
@@ -15,9 +18,14 @@ link_checkpoint ytoowqgj-1773645017 ytoowqgj data/mppo-dna-heads/
 #   0   command finished
 #   124 command timed out
 #   other = command failed
-command="python -m rl.algos.mppo_dna_gnn.mppo_dna_gnn --dry-run --rollouts 5 --skip-eval"
-output=$(timeout -f $((SECONDS_MAX * 2)) $command)
-[ $? -eq 0 ] || { echo "CHECK FAILED (timeout)"; exit 1; }
+command="python -m rl.algos.mppo_dna_gnn.mppo_dna_gnn --dry-run --max-rollouts $N_ROLLOUTS --skip-eval"
+set +e
+output=$(timeout --foreground $SECONDS_TIMEOUT $command)
+if [ $? -nq 0 ]; then
+  echo "CHECK FAILED (timeout)"
+  echo 0 > /checkresult
+  exit 1
+fi
 
 if printf "%s" "$output" | grep '"event": "finish"' | jq -e ".message.timers.all < $SECONDS_MAX"; then
   echo "CHECK PASSED"
