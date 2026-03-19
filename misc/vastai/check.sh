7600a7b2b99ce30c1eba56a0cd1dfc389a8f4a1d
Simeon Manolov <s.manolloff@gmail.com>
2026-03-19 12:56:56 +0200

    update check

--- misc/vastai/check.sh
+++ misc/vastai/check.sh
@@ -5,11 +5,18 @@ set -euxo pipefail
 TAG=false   # -t
 DEL=false   # -d
 
-while getopts "td" opt; do
+INIT_SECONDS=90     # cold start (40 envs, load weights, etc.)
+ROLLOUT_SECONDS=27  # "ok" duration of 1 rollout
+N_ROLLOUTS=5
+
+while getopts "tdi:r:" opt; do
     case "$opt" in
     t) TAG=true ;;
     d) DEL=true ;;
-    *) echo "Usage: $0 [-td]"; exit 1 ;;
+    i) INIT_SECONDS=$OPTARG ;;
+    r) ROLLOUT_SECONDS=$OPTARG ;;
+    n) N_ROLLOUTS=$OPTARG ;;
+    *) echo "Usage: $0 [-td] [-i INT] [-r INT] [-n INT]"; exit 1 ;;
     esac
 done
 
@@ -19,52 +26,60 @@ function http() {
     -X "$1" --json "$2"
 }
 
-N_ROLLOUTS=5
-INIT_SECONDS=90     # cold start (40 envs, load weights, etc.)
-ROLLOUT_SECONDS=27  # "ok" duration of 1 rollout
-
 SECONDS_CHECK=$((N_ROLLOUTS * ROLLOUT_SECONDS))
 SECONDS_TIMEOUT=$((INIT_SECONDS + SECONDS_CHECK * 2))
 
 CHECKPOINT=ytoowqgj-1773645017
 RUN_ID=${CHECKPOINT%-*}
 
+function check() {
+  . ~/.simorc
+  cd /workspace/vcmi-gym
+
+  if ! [ -f data/mppo-dna-heads/$CHECKPOINT-model-dna.pt ]; then
+    download_checkpoint ytoowqgj-1773645017
+  fi
+
+  link_checkpoint -y ytoowqgj-1773645017 ytoowqgj data/mppo-dna-heads/
+
+  # timeout returns:
+  #   0   command finished
+  #   124 command timed out
+  #   other = command failed
+  command="python -m rl.algos.mppo_dna_gnn.mppo_dna_gnn --dry-run --max-rollouts $N_ROLLOUTS --skip-eval -f data/mppo-dna-heads/$CHECKPOINT-config.json"
+  date
+  # Temporary disable -e to capture non-0 exit statuses without aborting
+  set +e
+  output=$(timeout --foreground $SECONDS_TIMEOUT $command)
+  status=$?
+  set -e
+  date
+  if [ $status -eq 124 ]; then
+    echo "CHECK FAILED (timeout)"
+    return $status
+  elif [ $status -ne 0 ]; then
+    echo "CHECK FAILED (error)"
+    return $status
+  fi
+
+  line=$(printf "%s" "$output" | grep '"event": "finish"') || :
+
+  if printf "%s" "$line" | jq -e ".message.timers.all < $SECONDS_CHECK"; then
+    echo "CHECK PASSED"
+    return 0
+  else
+    echo "CHECK FAILED (slow)"
+    return 1
+  fi
+}
+
 $TAG && http PUT '{"label": "check..."}' || :
 
-. ~/.simorc
-cd /workspace/vcmi-gym
-
-if ! [ -f data/mppo-dna-heads/$CHECKPOINT-model-dna.pt ]; then
-  download_checkpoint ytoowqgj-1773645017
-fi
-
-link_checkpoint -y ytoowqgj-1773645017 ytoowqgj data/mppo-dna-heads/
-
-# timeout returns:
-#   0   command finished
-#   124 command timed out
-#   other = command failed
-command="python -m rl.algos.mppo_dna_gnn.mppo_dna_gnn --dry-run --max-rollouts $N_ROLLOUTS --skip-eval -f data/mppo-dna-heads/$CHECKPOINT-config.json"
-set +e
-date
-output=$(timeout --foreground $SECONDS_TIMEOUT $command)
-status=$?
-date
-if [ $status -ne 0 ]; then
-  echo "CHECK FAILED (timeout)"
-  echo 0 > /checkresult
-  exit $status
-fi
-
-line=$(printf "%s" "$output" | grep '"event": "finish"') || :
-
-if printf "%s" "$line" | jq -e ".message.timers.all < $SECONDS_CHECK"; then
-  echo "CHECK PASSED"
-  $TAG && http PUT '{"label": "passed"}' || :
-  echo 1 > /checkresult
+if check; then
+    $TAG && http PUT '{"label": "passed"}' || :
+    echo 1 > /checkresult
 else
-  echo "CHECK FAILED"
-  $TAG && http PUT '{"label": "failed"}' || :
-  $DEL && http DELETE '{}' || :
-  echo 0 > /checkresult
+    $TAG && http PUT '{"label": "failed"}' || :
+    $DEL && http DELETE '{}' || :
+    echo 0 > /checkresult
 fi
