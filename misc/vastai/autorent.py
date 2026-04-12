8a4134ce14eb3e9be5dcd895323548db3622a28e
Simeon Manolov <s.manolloff@gmail.com>
2026-04-12 21:42:35 +0300

    fix autorent

--- misc/vastai/autorent.py
+++ misc/vastai/autorent.py
@@ -140,7 +140,7 @@ def vastai_rent(offer_id: int) -> int:
             r'cd\\\ /workspace\\\;'
             r'bash\\\ init.sh\\\;'
             r'bash\\\ check.sh\\\ -t\\\ -i90\\\ -r28\\\ -n5\;'
-            r'touch\ /workspace/.preinit \;'
+            r'touch\ /workspace/.preinit\;'
             r'exec\ \$SHELL'
         )
     )
